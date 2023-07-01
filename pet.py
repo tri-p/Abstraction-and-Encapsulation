468f72f7c7e7128b1c0bb5c05c0ce13695af4448
Katrina Pabuna <kinapabuna@gmail.com>
2023-07-01 20:52:20 +0800

    create class pet and add init method

--- pet.py
+++ pet.py
@@ -2,12 +2,16 @@
 # BSCPE 1-4
 
 # create class
+class Pet():
     # init method
+    def __init__(self):
         # private members
+        self.__name = ""
+        self.__animal_type = ""
+        self.__age = 0
 
     # def show
 
     # getter methods
 
-
-# ===== start =====
+    # setter methods
