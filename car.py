2493c15478e05aabcf64967d237a0fd3ba54e33e
Katrina Pabuna <kinapabuna@gmail.com>
2023-07-01 19:11:33 +0800

    create class car and add init method

--- car.py
+++ car.py
@@ -2,13 +2,14 @@
 # BSCPE 1-4
 
 # create class
+class Car:
     # init method
+    def __init__(self, year_model, make):
         # private members
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0  # default at zero
 
     # def show
 
     # methods
-
-# object 
-
-# ===== start =====
