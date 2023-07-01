ea43058cb8295a9c7ddf19617d9b217a63477687
Katrina Pabuna <kinapabuna@gmail.com>
2023-07-01 14:57:04 +0800

    new file

---
+++ fan.py
@@ -0,0 +1,16 @@
+# PABUNA, KATRINA B. 
+# BSCPE 1-4
+
+# create class
+    # init method
+        # private members
+
+    # def show
+
+    # getter methods
+
+    # setter methods to modify data member
+
+# object 
+
+# ===== start =====
--- test_fan.py
+++ test_fan.py
@@ -1,16 +1,1 @@
-# PABUNA, KATRINA B. 
-# BSCPE 1-4
-
-# create class
-    # init method
-        # private members
-
-    # def show
-
-    # getter methods
-
-    # setter methods to modify data member
-
-# object 
-
 # ===== start =====
