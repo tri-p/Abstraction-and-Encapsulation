7437575ff836b9e0a3e19cdb8c5ecb8ff7873e59
Katrina Pabuna <kinapabuna@gmail.com>
2023-07-01 20:58:11 +0800

    def show

--- pet.py
+++ pet.py
@@ -11,6 +11,10 @@ class Pet():
         self.__age = 0
 
     # def show
+    def show(self):
+        print("Name of the pet:", self.__name,
+              "\nAnimal type of the pet:", self.__animal_type,
+              "\nAge of pet:", self.__age)
 
     # getter methods
 
