79cff73fba77f0b5f9959bcc496041398ec03d3f
Katrina Pabuna <kinapabuna@gmail.com>
2023-07-01 18:36:19 +0800

    setter methods for fan 2

--- test_fan.py
+++ test_fan.py
@@ -14,3 +14,11 @@ default_fan.set_on(True)
 default_fan.set_radius(10)
 default_fan.set_color("Yellow")
 default_fan.show()
+
+# fan 2
+print("\nFAN 2")
+default_fan.set_speed(2)
+default_fan.set_on(False)
+default_fan.set_radius(5)
+default_fan.set_color("Blue")
+default_fan.show()
