4b0b6dbc94d1622b53cc82a927241c606452f46d
Katrina Pabuna <kinapabuna@gmail.com>
2023-07-01 18:21:24 +0800

    setter method for speed, fan 1

--- test_fan.py
+++ test_fan.py
@@ -3,10 +3,11 @@ from fan import Fan
 # ===== start =====
 
 # default fan
+print("\nDEFAULT FAN")
 default_fan = Fan("SLOW", "off", 5, "Blue")
 default_fan.show()
 
 # fan 1
-print("\n")
+print("\nFAN 1")
 default_fan.set_speed(3)
 default_fan.show()
